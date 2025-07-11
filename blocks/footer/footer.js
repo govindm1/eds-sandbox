071ca1d131f5d8d54d8d8691740b06cd11b0b590
Ratna1007 <126454864+Ratna1007@users.noreply.github.com>
2025-07-12 00:02:38 +0530

    Update footer.js

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,4 +1,5 @@
 import { getMetadata } from '../../scripts/aem.js';
+import { getSiteRoot } from '../../scripts/scripts.js';
 import { loadFragment } from '../fragment/fragment.js';
 
 /**
@@ -8,7 +9,7 @@ import { loadFragment } from '../fragment/fragment.js';
 export default async function decorate(block) {
   // load footer as fragment
   const footerMeta = getMetadata('footer');
-  const footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : '/footer';
+  const footerPath = footerMeta ? new URL(footerMeta).pathname : `${getSiteRoot()}/footer`;
   const fragment = await loadFragment(footerPath);
 
   // decorate footer DOM
