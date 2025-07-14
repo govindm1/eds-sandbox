79b5b5ffc691ff4308f1570f6c356c4167cd7405
Eepsita Chakraborty <eepsita04@gmail.com>
2025-07-14 16:19:16 +0530

    Update footer.js

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -7,37 +7,20 @@ import { loadFragment } from '../fragment/fragment.js';
  * @param {Element} block The footer block element
  */
 export default async function decorate(block) {
-  // load footer as fragment
-  const footerMeta = getMetadata('footer');
-  const footerPath = footerMeta ? new URL(footerMeta).pathname : `${getSiteRoot()}/footer`;
-  const fragment = await loadFragment(footerPath);
+    // load footer as fragment
+    const footerMeta = getMetadata('footer');
+    const footerPath = footerMeta ? new URL(footerMeta).pathname : `${getSiteRoot()}/footer`;
+    const fragment = await loadFragment(footerPath);
 
-  // decorate footer DOM
-  block.textContent = '';
-  const footer = document.createElement('div');
-  while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
+    // decorate footer DOM
+    block.textContent = '';
+    const footer = document.createElement('div');
+    while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
 
-  const classes = ['brand', 'sections', 'tools'];
-  classes.forEach((c, i) => {
-    const section = footer.children[i];
-    if (section) {
-      section.classList.add(`footer-${c}`);
-
-      const h5 = section.querySelector('h5');
-      if (h5) {
-        const wrapper = document.createElement('div');
-        wrapper.classList.add('footer-subsection');
-
-        let sibling = h5;
-        while (sibling) {
-          const next = sibling.nextElementSibling;
-          wrapper.appendChild(sibling);
-          sibling = next;
-        }
-
-        section.appendChild(wrapper);
-      }
-    }
-  });
-  block.append(footer);
+    const classes = ['brand', 'sections', 'tools'];
+    classes.forEach((c, i) => {
+        const section = footer.children[i];
+        if (section) section.classList.add(`footer-${c}`);
+    });
+    block.append(footer);
 }
