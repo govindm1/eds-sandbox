469d194845b00d033969f8d3cadc84048e711381
Eepsita Chakraborty <eepsita04@gmail.com>
2025-07-20 18:34:54 +0530

    Update custom-block.js

--- blocks/custom-block/custom-block.js
+++ blocks/custom-block/custom-block.js
@@ -1,14 +1,18 @@
-
-    
-  console.log('Script is loaded');
-
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('DOM ready');
-
-  const block = document.querySelector('.custom-block.block[data-block-name="custom-block"]');
-  console.log('Block found:', block);
-
-  if (!block) {
-    console.warn('Block not found at DOMContentLoaded');
+function init() {
+    console.log('DOM ready');
+  
+    const block = document.querySelector('.custom-block.block[data-block-name="custom-block"]');
+    console.log('Block found:', block);
+  
+    if (!block) {
+      console.warn('Block not found at DOMContentLoaded');
+    }
   }
-});
+  
+  if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', init);
+  } else {
+   
+    init();
+  }
+  
