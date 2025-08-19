f2338fd51bd661e20d2f4fc108d7b51fcd68679d
Eepsita Chakraborty <eepsita04@gmail.com>
2025-08-19 17:10:28 +0530

    Update columns.js

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -4,16 +4,28 @@ export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
 
-  // setup image columns
   [...block.children].forEach((row) => {
     [...row.children].forEach((col) => {
       const pic = col.querySelector('picture');
       const img = pic?.querySelector('img');
 
       if (pic && img) {
-        const imageLink = img.dataset.imageLink || '';
-        const optimizedPic = createOptimizedPicture(imageLink, img.src, img.alt);
-        pic.replaceWith(optimizedPic); 
+        const imgSrc = img.src;
+        const imgAlt = img.alt;
+        const imageLink = img.dataset.imageLink?.trim();
+
+        const optimizedPic = createOptimizedPicture(imgSrc, imgAlt);
+        console.log('Image Source:', imgSrc);
+        console.log('Alt Text:', imgAlt);
+        console.log('Image Link (from JSON):', imageLink);
+        if (imageLink) {
+          const link = document.createElement('a');
+          link.href = imageLink;
+          link.appendChild(optimizedPic);
+          pic.replaceWith(link);
+        } else {
+          pic.replaceWith(optimizedPic);
+        }
       }
 
       if (pic && col.children.length === 1) {
