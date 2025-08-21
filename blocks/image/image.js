df2236b6690126d35535a663c7f7a45a6833a07b
Eepsita Chakraborty <eepsita04@gmail.com>
2025-08-21 20:36:41 +0530

    deleting files for image

--- blocks/image/image.js
+++
@@ -1,7 +0,0 @@
-
-
-export default function decorate(block) {
-  block.querySelector('picture').classList.add('image-wrapper-test');
-  console.log('[createOptimizedPicture] image js 1:', image_url);
-  
-}
