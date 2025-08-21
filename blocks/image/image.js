1069fbaf8083398026958affb0c8b210b8a23d22
Eepsita Chakraborty <eepsita04@gmail.com>
2025-08-21 19:59:59 +0530

    pushing changes

---
+++ blocks/image/image.js
@@ -0,0 +1,7 @@
+
+
+export default function decorate(block) {
+  block.querySelector('picture').classList.add('image-wrapper-test');
+  console.log('[createOptimizedPicture] image js 1:', image_url);
+  
+}
