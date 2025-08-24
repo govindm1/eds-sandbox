6306021523be9457b58d766894e8e742c18789a0
Eepsita Chakraborty <eepsita04@gmail.com>
2025-08-24 20:34:48 +0530

    Update columns.js

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -4,20 +4,37 @@ export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
 
-  // setup image columns
   [...block.children].forEach((row) => {
     [...row.children].forEach((col) => {
       const pic = col.querySelector('picture');
+    
       if (pic) {
-        const img = pic.querySelector('img');
-        if (img) {
-          const optimizedPic = createOptimizedPicture(img.src, img.alt || '', false, [{ width: '750' }]);
-          pic.replaceWith(optimizedPic);
-        }
         const picWrapper = pic.closest('div');
         if (picWrapper && picWrapper.children.length === 1) {
-          // picture is only content in column
+          console.log('Inside picwrapper');
+          
           picWrapper.classList.add('columns-img-col');
+          const img = pic.querySelector('img');
+          const imgSrc = img?.src;
+          const imgAlt = img?.alt || '';
+          const imageLink = pic.getAttribute('imagelink');
+    
+          console.log('Image Source:', imgSrc);
+          console.log('Alt Text:', imgAlt);
+          console.log('Image Link:', imageLink);
+    
+          pic.parentElement.innerHTML = ''; 
+    
+          const optimizedPic = createOptimizedPicture(imgSrc, imgAlt, imageLink);
+    
+          if (imageLink) {
+            const link = document.createElement('a');
+            link.href = imageLink;
+            link.appendChild(optimizedPic);
+            col.appendChild(link);
+          } else {
+            col.appendChild(optimizedPic);
+          }
         }
       }
     });
