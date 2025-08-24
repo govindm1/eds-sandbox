69bdddcd22f2da9804d91d63cf3aada337ff05ea
Eepsita Chakraborty <eepsita04@gmail.com>
2025-08-24 20:14:34 +0530

    Update columns.js

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,40 +1,16 @@
-import { createOptimizedPicture } from '../../scripts/aem.js';
-
 export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
 
+  // setup image columns
   [...block.children].forEach((row) => {
     [...row.children].forEach((col) => {
       const pic = col.querySelector('picture');
-    
       if (pic) {
         const picWrapper = pic.closest('div');
         if (picWrapper && picWrapper.children.length === 1) {
-          console.log('Inside picwrapper');
-          
+          // picture is only content in column
           picWrapper.classList.add('columns-img-col');
-          const img = pic.querySelector('img');
-          const imgSrc = img?.src;
-          const imgAlt = img?.alt || '';
-          const imageLink = pic.getAttribute('imagelink');
-    
-          console.log('Image Source:', imgSrc);
-          console.log('Alt Text:', imgAlt);
-          console.log('Image Link:', imageLink);
-    
-          pic.parentElement.innerHTML = ''; 
-    
-          const optimizedPic = createOptimizedPicture(imgSrc, imgAlt, imageLink);
-    
-          if (imageLink) {
-            const link = document.createElement('a');
-            link.href = imageLink;
-            link.appendChild(optimizedPic);
-            col.appendChild(link);
-          } else {
-            col.appendChild(optimizedPic);
-          }
         }
       }
     });
